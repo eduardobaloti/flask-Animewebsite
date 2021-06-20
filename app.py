f5e27c07d966e5ddf9350b9b5f2b870ec03f05d7
eduardobaloti <g.eduardobaloti@gmail.com>
2021-06-20 16:00:53 -0300

    Api fixed and layout finishsed

--- app.py
+++ app.py
@@ -8,14 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
-@app.route('/anime', methods=['POST'])
-# def anime():
-#    zipcode = request.form['zip']
-#    r = requests.get("https://jikan1.p.rapidapi.com/search/anime")
-#    json_object = r.json()
-#    anime = float(json_object['main']['temp'])
-#    return render_template('anime.html', temp=temp_k)
+@app.route('/list', methods=['POST'])
 def anime():
     headers = {
     'x-rapidapi-key': "3d90e7b795mshdcbf95b0e658220p15126fjsn6bec6924f199",
@@ -25,9 +18,8 @@ def anime():
     anime = request.form['animename']
     querystring = {"q":{anime}}
     response = requests.request("GET", url, headers=headers, params=querystring)
-    json_object = response.json()
-    animereq = str(json_object['title'])
-    return render_template('anime.html', animere=animereq)
+    data = response.json()
+    return render_template("list.html", data=data)
 
 
 if __name__ == '__main__':
